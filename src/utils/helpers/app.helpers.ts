73a8525bc22b80d03334600c05fd1e3570147767
HugoCamposLopez <hugo.antonio.campos@gmail.com>
2022-11-07 15:26:00 -0600

    (ADD) Dropdown component and storybook.

---
+++ src/utils/helpers/app.helpers.ts
@@ -0,0 +1,7 @@
+export const numberOfPlayers = [
+  {value: "2", label: "2 jugadores"},
+  {value: "3", label: "3 jugadores"},
+  {value: "4", label: "4 jugadores"},
+  {value: "5", label: "5 jugadores"},
+  {value: "6", label: "6 jugadores"},
+]
