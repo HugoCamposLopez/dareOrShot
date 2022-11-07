b8724baf8895894efc8bbd4367008fec15d2adcc
HugoCamposLopez <hugo.antonio.campos@gmail.com>
2022-11-07 13:08:38 -0600

    (ADD) onChange prop

--- src/components/RSInputText/RSInputText.stories.tsx
+++ src/components/RSInputText/RSInputText.stories.tsx
@@ -1,4 +1,5 @@
 import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
+import { useCallback, useState } from 'react'
 import withProviders from '../../utils/withProviders'
 import { RSInputText, RSInputTextProps } from './RSInputText'
 
@@ -10,14 +11,49 @@ export default {
 export const DefaultInputText: ComponentStoryObj<typeof RSInputText> = ({
   placeholder,
   isError,
-}: RSInputTextProps) =>
-  withProviders(<RSInputText isError={isError} placeholder={placeholder} />)
+  onChange,
+  value,
+}: RSInputTextProps) => {
+  const [value1, setValue] = useState<any>('')
 
+  const onChangeHandler = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(e.target.value)
+    },
+    [setValue]
+  )
+
+  return withProviders(
+    <RSInputText
+      value={value1}
+      onChange={onChangeHandler}
+      isError={isError}
+      placeholder={placeholder}
+    />
+  )
+}
 export const ErrorInputText: ComponentStoryObj<typeof RSInputText> = ({
   placeholder,
   isError,
-}: RSInputTextProps) =>
-  withProviders(<RSInputText isError={isError} placeholder={placeholder} />)
+  onChange,
+}: RSInputTextProps) => {
+  const [value1, setValue] = useState<any>('')
+
+  const onChangeHandler = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(e.target.value)
+    },
+    [setValue]
+  )
+  return withProviders(
+    <RSInputText
+      isError={isError}
+      value={value1}
+      placeholder={placeholder}
+      onChange={onChangeHandler}
+    />
+  )
+}
 
 DefaultInputText.args = {
   placeholder: 'component.text.placeholder',
--- src/components/RSInputText/RSInputText.tsx
+++ src/components/RSInputText/RSInputText.tsx
@@ -5,13 +5,22 @@ import './styles.scss'
 export interface RSInputTextProps {
   placeholder: string
   isError: boolean
+  value?: string
+  onChange?: React.ChangeEventHandler<HTMLInputElement>
 }
-export const RSInputText = ({ placeholder, isError }: RSInputTextProps) => {
+export const RSInputText = ({
+  placeholder,
+  isError,
+  onChange,
+  value,
+}: RSInputTextProps) => {
   const intl = useIntl()
   return (
     <div className={`text-input-container ${isError ? 'error' : ''}`}>
       <p>{intl.formatMessage({ id: 'component.text.name' })}</p>
       <TextInput
+        value={value}
+        onChange={onChange}
         error={isError}
         icon={
           <Image width={25} height={25} src="./icons/glass-user-icon.svg" />
